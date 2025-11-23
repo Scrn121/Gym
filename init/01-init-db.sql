9aeff572e0b24d8042127108893be6cd9672df77
Aleksandr <alexanderborodin.1995@gmail.com>
2025-11-24 00:00:23 +0300

    structure

--- init/01-init-db.sql
+++ init/01-init-db.sql
@@ -2,16 +2,16 @@ create schema if not exists gym;
 
 create table if not exists gym.exercises (
 	ID serial primary key,
-	Name varchar(255) null
+	Name varchar(255) null unique
 );
 
 create table if not exists gym.workouts(
-	ID serial primary keym
+	ID serial primary key,
 	Exercise_ID int not null references gym.exercises(ID) on delete cascade,
 	Workout_date date not null,
 	sets int not null check(sets > 0),
 	reps int not null check(reps > 0),
 	weight_kg numeric(5,2) not null check(weight_kg >= 0),
-	volume_kg as (sets * reps * weight_kg) stored,
+	volume_kg numeric generated always as (sets * reps * weight_kg) stored,
 	created_at timestamptz default now()
 )
